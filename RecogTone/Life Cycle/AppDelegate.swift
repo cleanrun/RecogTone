eab90d935df27381e2e4db1675abdd70d6f391df
Marchell <marchellehcram@gmail.com>
2021-10-15 18:42:41 +0700

    Initial Commit

---
+++ RecogTone/Life Cycle/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  RecogTone
+//
+//  Created by cleanmac on 03/10/21.
+//
+
+import Foundation
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+}
---
+++ RecogTone/Life Cycle/RecogToneApp.swift
@@ -0,0 +1,19 @@
+//
+//  RecogToneApp.swift
+//  RecogTone
+//
+//  Created by cleanmac on 03/10/21.
+//
+
+import SwiftUI
+
+@main
+struct RecogToneApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
